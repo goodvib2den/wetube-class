07a0a4acb9f938c3141e5c96986d5a97706c71ae
eden <gmean7@gmail.com>
2022-07-25 18:51:01 +0900

    express를 통해  get 함수 추가하여 req 이해하기

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("누군가 접근을 시도중입니다.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✅Server Litening on port http://localhost:${PORT}`);
 
